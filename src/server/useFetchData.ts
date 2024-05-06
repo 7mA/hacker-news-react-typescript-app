e0d8c529535de3daeb25ee86392ffd4ed2e34983
7mA <812966201@qq.com>
2024-05-06 13:35:24 +0900

    [fix]optimize useFetchData.js

--- src/server/useFetchData.ts
+++ src/server/useFetchData.ts
@@ -12,52 +12,42 @@ function useFetchData(sentinelRef: React.RefObject<HTMLDivElement>) {
   const pageSize = 20;
   const firstViewSize = 100;
 
-  useEffect(() => {
-    const fetchKeyList = async () => {
-      let keyList: number[] = [];
-      if (keyListCache.current) {
-        keyList = keyListCache.current;
-      } else {
-        const keyResponse = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty');
-        keyList = keyResponse.data;
-        keyListCache.current = keyList;
-      }
+  const fetchKeyList = async () => {
+    let keyList: number[] = [];
+    if (keyListCache.current) {
+      keyList = keyListCache.current;
+    } else {
+      const keyResponse = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty');
+      keyList = keyResponse.data;
+      keyListCache.current = keyList;
     }
+  }
 
-    const fetchData = async (start: number) => {
-      try {
-        setLoading(true);
-        let keyList = keyListCache.current ?? [];
-        const limitedKeyList = keyList.slice(start, start + pageSize);
+  const fetchData = async (start: number) => {
+    try {
+      setLoading(true);
+      let keyList = keyListCache.current ?? [];
+      const limitedKeyList = keyList.slice(start, start + pageSize);
 
-        const promises = limitedKeyList.map(key => axios.get(`https://hacker-news.firebaseio.com/v0/item/${key}.json?print=pretty`));
-        const detailResponseList = await Promise.all(promises);
+      const promises = limitedKeyList.map(key => axios.get(`https://hacker-news.firebaseio.com/v0/item/${key}.json?print=pretty`));
+      const detailResponseList = await Promise.all(promises);
 
-        setData(prevData => {
-          const newData = [...prevData ?? [], ...detailResponseList.map(response => response.data)];
-          return newData.filter((item, index, self) => index === self.findIndex(t => t.id === item.id));
-        });
-      } catch (err) {
-        if (axios.isAxiosError(err)) {
-          setError(err);
-        } else {
-          console.error('予期しないエラー', err);
-        }
-      } finally {
-        setLoading(false);
+      setData(prevData => {
+        const newData = [...prevData ?? [], ...detailResponseList.map(response => response.data)];
+        return newData.filter((item, index, self) => index === self.findIndex(t => t.id === item.id));
+      });
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        setError(err);
+      } else {
+        console.error('予期しないエラー', err);
       }
-    };
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    fetchKeyList().then(() => {
-      if (!startCount.current) {
-        startCount.current = 0;
-      }
-      while (startCount.current < firstViewSize) {
-        fetchData(startCount.current);
-        startCount.current += pageSize;
-      }
-    });
-    
+  useEffect(() => {
     const observerCallback = (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
       if (entries[0].isIntersecting && !loading && startCount.current && keyListCache.current?.length) {
         if (startCount.current < keyListCache.current?.length) {
@@ -79,7 +69,21 @@ function useFetchData(sentinelRef: React.RefObject<HTMLDivElement>) {
         observer.unobserve(sentinelRef.current);
       }
     };
-  }, [loading, sentinelRef]);
+  }, [loading, sentinelRef])
+
+  useEffect(() => {
+    fetchKeyList().then(() => {
+      if (!startCount.current) {
+        startCount.current = 0;
+      }
+      while (startCount.current < firstViewSize) {
+        fetchData(startCount.current);
+        startCount.current += pageSize;
+      }
+    });
+    
+    
+  }, []);
 
   return { data, loading, error };
 }
